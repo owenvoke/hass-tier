1b6cf26f8c256a14355bc5502f2040842d488af8
Owen Voke <development@voke.dev>
2023-07-05 09:56:12 +0100

    feat: add support for updating interval

--- custom_components/tier/config_flow.py
+++ custom_components/tier/config_flow.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from datetime import timedelta
 from hashlib import md5
+import logging
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
 from homeassistant.const import (
     CONF_API_TOKEN,
     CONF_SCAN_INTERVAL,
@@ -11,12 +13,15 @@ from homeassistant.const import (
     CONF_LONGITUDE,
     CONF_RADIUS,
 )
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from tier import TIER, NotFoundException, UnauthorizedException
 
 import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
 class TIERConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -76,6 +81,53 @@ class TIERConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user", data_schema=config_schema, errors=errors
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> TIEROptionsFlowHandler:
+        return TIEROptionsFlowHandler(config_entry)
+
+
+class TIEROptionsFlowHandler(OptionsFlow):
+    """Config flow options handler for TIER."""
+
+    def __init__(self, config_entry: ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+        # Cast from MappingProxy to dict to allow update.
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            self.options.update(user_input)
+            coordinator = self.hass.data[DOMAIN][self.config_entry.entry_id]
+
+            update_interval = timedelta(
+                seconds=self.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+            )
+
+            _LOGGER.debug("Updating coordinator, update_interval: %s", update_interval)
+
+            coordinator.update_interval = update_interval
+
+            return self.async_create_entry(title="", data=self.options)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL,
+                        default=self.config_entry.options.get(
+                            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+                        ),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=1)),
+                }
+            ),
         )
 
 
